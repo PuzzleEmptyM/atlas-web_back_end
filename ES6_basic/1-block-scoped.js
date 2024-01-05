1220d9ce3326d42f13f23e3faa9b19f27bddc9eb
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-01-05 17:22:08 -0600

    don't use let

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    let task = true;  // Block-scoped, separate from the outer 'task'
-    let task2 = false; // Block-scoped, separate from the outer 'task2'
+    (function() {
+      var task = true;  // This 'task' is scoped to the IIFE
+      var task2 = false; // This 'task2' is scoped to the IIFE
+    })();
   }
 
   return [task, task2]; // Always returns the outer scope variables
