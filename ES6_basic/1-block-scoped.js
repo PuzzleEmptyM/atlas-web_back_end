df0970b492f045907a3c79d7cdd0906488d33567
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-01-05 17:40:42 -0600

    confusion

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,13 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
     (function() {
-      var task = true;  // This 'task' is scoped to the IIFE
-      var task2 = false; // This 'task2' is scoped to the IIFE
+      var task = true;
+      var task2 = false;
     })();
   }
 
-  return [task, task2]; // Always returns the outer scope variables
+  return [task, task2];
 }
