6eeceb746901a200cff9f7fdad813bafa8add7fa
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-01-05 21:10:34 -0600

    linter

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,10 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let newArray = [];
+  const newArray = [];
   let index = 0;
 
   for (const value of array) {
     newArray[index] = appendString + value;
-    index++;
+    index += 1;
   }
 
   return newArray;
