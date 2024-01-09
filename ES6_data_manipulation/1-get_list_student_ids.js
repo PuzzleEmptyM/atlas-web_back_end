b4f1e90ac6e84bbe419c9e35b343589d18fe0af3
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-01-09 13:29:59 -0600

    fixed lint

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,7 +2,7 @@ function getListStudentIds(array) {
   if (!Array.isArray(array)) {
     return [];
   }
-  return array.map(student => student.id);
+  return array.map((student) => student.id);
 }
 
 export default getListStudentIds;
