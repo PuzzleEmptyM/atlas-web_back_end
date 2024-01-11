d8da51715ed53804be740836ccc33f790b8ae3ce
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-01-10 23:47:00 -0600

    Knowing that the functions in utils.js return promises, use the prototype below to collectively resolve all promises and log body firstName lastName to the console.

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import uploadPhoto from './utils';
+import createUser from './utils';
+
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
+
+export default handleProfileSignup;
