890a5b2300f6753d298f9d5ed5a6d210c6df29f6
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-01-10 23:52:06 -0600

    what is happening??? why failing all checks??

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,5 +1,4 @@
-import uploadPhoto from './utils';
-import createUser from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((response) => {
