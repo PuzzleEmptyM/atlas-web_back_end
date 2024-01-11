d6b2e5f3453a85cc0dff7496d0e8512057273fe8
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-01-10 23:10:54 -0600

    actually using resolve and reject

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,4 +1,12 @@
-export default function getResponseFromAPI() {
+function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
+    const condition = true; 
+    if (condition) {
+      resolve();
+    } else {
+      reject();
+    }
   });
 }
+
+export default getResponseFromAPI;
