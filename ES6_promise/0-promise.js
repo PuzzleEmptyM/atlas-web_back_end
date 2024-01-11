88454107d06dd59ffc8c555069d5913968ed6aee
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-01-10 23:13:45 -0600

    white space

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,6 @@
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    const condition = true; 
+    const condition = true;
     if (condition) {
       resolve();
     } else {
