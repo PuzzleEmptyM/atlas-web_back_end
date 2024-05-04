8071ab26e7227268ea1c6178c782deae31553fd1
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-04 12:17:50 -0500

    adding strings together (concat)

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,2 +1,14 @@
-def add(a: float, b: float) -> float:
-    return a + b
+#!/usr/bin/env python3
+"""
+a type-annotated function concat that takes a string str1 and a string str2 as
+arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    str1: first string
+    str1: second string
+    return: full string composed of both strings
+    """
+    return str1 + str2
