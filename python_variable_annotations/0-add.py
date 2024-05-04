06c08966d8077e778599d8c74bec17aff2823429
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-04 12:12:51 -0500

    documentation??

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-
+"""type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float"""
 
 def add(a: float, b: float) -> float:
+    """
+    arg1: a - first float
+    arg2: b - second float
+    return: sum of args
+    """
     return a + b
