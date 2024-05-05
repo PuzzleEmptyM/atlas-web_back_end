d8b1f4a4ce00e0a4118178ec090ed9abe33d3089
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-05 17:15:27 -0500

    function to compute sum of a list of floats

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+A functino to compute the sum of a list of floats
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    arg: input_list - float
+    return: float - sum of the input list
+    """
+    return sum(input_list)
