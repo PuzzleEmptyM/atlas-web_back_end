a101d32707116de417376ad03616559b04bddcab
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-05 17:58:01 -0500

    update

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -4,7 +4,7 @@ Module to define variables with annotations
 """
 
 
-a: int = 1
-pi: float = 3.14
-i_understand_annotations: bool = True
-school: str = "Holberton"
+a: int = 1  # An integer with a value of 1
+pi: float = 3.14  # A float with a value of 3.14
+i_understand_annotations: bool = True  # A bool with a value of True
+school: str = "Holberton"  # A string with a value of "Holberton"
