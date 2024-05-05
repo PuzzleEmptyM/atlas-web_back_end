813a3c0c57d1f49957c89a41b566e4299dbd5cd3
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-05 18:53:50 -0500

    new change in syntax

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,21 +3,21 @@
 This module contains an asynchronous coroutine that runs another coroutine
 multiple times concurrently.
 """
+from typing import List
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Runs wait_random n times concurrently with the specified max_delay.
-
+    Runs wait_random n times concurrently with the specified max_delay and
+    returns a list of delays.
     Args:
     n (int): The number of times to run wait_random.
     max_delay (int): The maximum delay for wait_random.
-
     Returns:
-    list: A list of float values representing the delays, ordered naturally
-    by concurrency.
+    List[float]: A list of float values representing the delays, ordered
+    naturally by concurrency.
     """
     tasks = [wait_random(max_delay) for _ in range(n)]
     completed_delays = []
