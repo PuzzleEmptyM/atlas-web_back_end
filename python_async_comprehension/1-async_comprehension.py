361aaa2b194f6ffcdeb03cf4aed338d7fb141628
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-06 14:46:31 -0500

    collects 10 numbers using async comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 """
-
+This module contains a coroutine called async_comprehension that
+collects 10 random numbers asynchonously using a comprehension over
+an async generator
 """
-import
+async_generator = __import__('0-async_generator').async_generator
 
 
-def
+async def async_comprehension() -> list:
     """
-
+    Asynchronously collects 10 random numbers using asynchronous comprehension
+    over an async generator
     """
-    return
+    return [x async for x in async_generator()]
