ba59d75aa75c491ad06b30ea959328c0cda7f1bc
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-06 14:03:33 -0500

    number generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 """
-
+This module contains an asynchronous coroutine called async_generator that
+generates random numbers asynchrounosly
 """
-import
+import asyncio
+import random
 
 
-def
+async def async_generator():
     """
-
+    This coroutine loops 10 times, waits asynchronously for 1 second
+    and yeilds a random number between 0 and 10 each iteration
     """
-    return
+    for _ in range(10):
+        await asyncio.sleep(1)  # Asynchronously wait for 1 second
+        yield random.uniform(0, 10)  # Yield a random number between 0 and 10
