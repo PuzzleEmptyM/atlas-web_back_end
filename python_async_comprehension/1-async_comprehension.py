420c5b750581eee172083205665f0daba6899629
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-06 14:48:40 -0500

    ADDED TYPING.LIST

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,10 +4,11 @@ This module contains a coroutine called async_comprehension that
 collects 10 random numbers asynchonously using a comprehension over
 an async generator
 """
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list:
+async def async_comprehension() -> List[float]:
     """
     Asynchronously collects 10 random numbers using asynchronous comprehension
     over an async generator
