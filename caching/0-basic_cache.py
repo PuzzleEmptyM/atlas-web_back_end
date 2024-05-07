1afcbf1f626f534670803cedd7d417b836f017f8
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-07 13:26:49 -0500

    fixed syntax error on line 29

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -26,6 +26,6 @@ class BasicCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        if key is not None or key not in self.cache_data:
+        if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
