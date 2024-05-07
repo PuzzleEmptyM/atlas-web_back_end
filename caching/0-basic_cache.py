ffeee2983feb2ac2f42f790cf819e569603897bd
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-07 14:07:33 -0500

    more documentation

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -10,10 +10,16 @@ class BasicCache(BaseCaching):
     BasicCache class inherits from BaseCaching and provides simple
     """
     def put(self, key, item):
+        """
+        Store item in cache_data using provided key
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """
+        Return item linked to key from cache_data
+        """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
