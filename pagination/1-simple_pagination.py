829f92124af92ea123237ed2165b83073f17a8a6
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-16 10:26:10 -0500

    fixed line too long

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -2,8 +2,8 @@
 """
 Write a function named index_range that takes two integer arguments page and
 page_size. The function should return a tuple of size two containing a start
-index and an end index corresponding to the range of indexes to return in a list
-for those particular pagination parameters.
+index and an end index corresponding to the range of indexes to return in a
+list for those particular pagination parameters.
 """
 import csv
 from typing import List, Tuple
