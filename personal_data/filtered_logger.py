147f2f1e830e7abbcd53031163bc160c343ea434
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-17 12:09:37 -0500

    properly annottated

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,12 +3,17 @@
 This function "filter_datum" returns a log message obfuscated
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """
-    Args: none
-    Return: 
+    Args: fields (list): fields to obfuscate
+          redaction (str): string to replace the field value
+          message (str): log message to be processed
+          separator (str): separator used in the log message
+    Return: (str): obfuscated log message
     """
     return re.sub(r'(' + '|'.join(fields) + r')=[^' + separator + r']*',
                   lambda m: m.group().split('=')[0] + '=' + redaction,
