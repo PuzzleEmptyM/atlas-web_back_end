73f6db8e3c71a5ed865308695808db27a5501fa9
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-23 09:51:51 -0500

    Implement a get_logger function that takes no arguments and returns a logging.Logger object.

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,19 +6,7 @@ import logging
 import re
 from typing import List
 
-
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """
-    Args: fields (list): fields to obfuscate
-          redaction (str): string to replace the field value
-          message (str): log message to be processed
-          separator (str): separator used in the log message
-    Return: (str): obfuscated log message
-    """
-    return re.sub(r'(' + '|'.join(fields) + r')=[^' + separator + r']*',
-                  lambda m: m.group().split('=')[0] + '=' + redaction,
-                  message)
+PII_FIELDS = ('email', 'phone_number', 'password', 'name', 'address')
 
 
 class RedactingFormatter(logging.Formatter):
@@ -53,3 +41,34 @@ class RedactingFormatter(logging.Formatter):
         original_message = super(RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION,
                             original_message, self.SEPARATOR)
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Args: fields (list): fields to obfuscate
+          redaction (str): string to replace the field value
+          message (str): log message to be processed
+          separator (str): separator used in the log message
+    Return: (str): obfuscated log message
+    """
+    return re.sub(r'(' + '|'.join(fields) + r')=[^' + separator + r']*',
+                  lambda m: m.group().split('=')[0] + '=' + redaction,
+                  message)
+
+
+def get_logger() -> logging.Logger:
+    """
+    Return: logging.Logger - logger configured to redact specific PII fields
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
