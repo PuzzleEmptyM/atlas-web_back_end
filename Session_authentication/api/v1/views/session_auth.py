5ca05afd1a7025c2dca781cbe465d3fda677d755
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-05-26 16:35:53 -0500

    pycode

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -39,7 +39,8 @@ def login():
     return response
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route(
+        '/auth_session/logout', methods=['DELETE'], strict_slashes=False)
 def logout():
     """Handles user logout for session auth"""
     from api.v1.app import auth
