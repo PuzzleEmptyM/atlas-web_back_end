c062468fb7eb00627482fa8ab8b46e53e7eed39e
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-05-26 17:14:41 -0500

    pycodestyle

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -37,7 +37,7 @@ class Auth:
         """
         Arg1: email - str; the email of the user to register
         Arg2: password - str; the password of the user to register
-        Return: User - newly created User object or 
+        Return: User - newly created User object or
                        raises ValueError if user already exists
         """
         try:
