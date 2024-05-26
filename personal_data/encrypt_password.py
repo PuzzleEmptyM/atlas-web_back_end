73f1b3049dd723cc0165e9e10bc25aab70c4dcc6
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-05-25 21:16:38 -0500

    returns True if the password matches the hashed password, and False otherwise.

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -14,3 +14,13 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Arg1: hashed_password - bytes type; the hashed password
+    Arg2: password - string type; the plain text password to check
+
+    Return: boolean - True if password matches, else False
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
