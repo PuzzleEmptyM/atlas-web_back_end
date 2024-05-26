ec394c32fad9ace550cc96dbf8bdd228e7a8e11f
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-05-26 15:07:29 -0500

    testing

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -5,7 +5,9 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import CORS
+from flask_cors import (CORS, cross_origin)
+import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -17,27 +19,28 @@ AUTH_TYPE = getenv("AUTH_TYPE", None)
 if AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif AUTH_TYPE == "session_auth":
-    from api.v1.auth.session_auth import SessionAuth
-    auth = SessionAuth()
 elif AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """ Forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
+
 
 @app.before_request
 def before_request():
@@ -45,24 +48,19 @@ def before_request():
     if auth is None:
         return
 
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/',
-        '/api/v1/auth_session/login/'
-    ]
-
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None\
-        and auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None:
         abort(401)
 
     request.current_user = auth.current_user(request)
 
     if request.current_user is None:
         abort(403)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
