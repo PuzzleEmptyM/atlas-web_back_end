4fe16e43b0ae791da528e24980470c92054bb790
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-05-26 13:38:01 -0500

    Retrieves the User instance for a requerst

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -92,3 +92,28 @@ class BasicAuth(Auth):
             return None
 
         return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the User instance for a request
+
+        Arg: request - Flask request object; request to process
+        Return: User instance or None if not found
+        """
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+
+        base64_auth = self.extract_base64_authorization_header(auth_header)
+        if base64_auth is None:
+            return None
+
+        decoded_auth = self.decode_base64_authorization_header(base64_auth)
+        if decoded_auth is None:
+            return None
+
+        user_email, user_password = self.extract_user_credentials(decoded_auth)
+        if user_email is None or user_password is None:
+            return None
+
+        return self.user_object_from_credentials(user_email, user_password)
