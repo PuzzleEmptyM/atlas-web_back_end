6c91db70da93201d812909910fedd636b3d193be
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-05-26 12:24:32 -0500

    fixed line too long

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -46,7 +46,8 @@ def before_request():
     if auth is None:
         return
 
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
 
