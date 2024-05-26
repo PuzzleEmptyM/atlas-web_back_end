038077a7e5634d05642a3e86f86a1886dae280dd
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-05-26 17:24:42 -0500

    reset password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -96,3 +96,16 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except NoResultFound:
             pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Arg1: email - str; the email of the user
+        Return: str - the reset password token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError(f"User {email} does not exist")
