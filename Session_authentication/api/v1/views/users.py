644c067f3efa7f3c099a4028ec05ef1d219d4a93
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-05-26 14:06:24 -0500

    add a new endpoint: GET /users/me to retrieve the authenticated User object.

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-""" Module of Users views
-"""
+""" Module of Users views """
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
@@ -25,12 +24,29 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
+    if user_id == "me":
+        if request.current_user is None:
+            abort(404)
+        return jsonify(request.current_user.to_json())
+
     if user_id is None:
         abort(404)
     user = User.get(user_id)
     if user is None:
         abort(404)
     return jsonify(user.to_json())
+
+
+@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
+def view_authenticated_user() -> str:
+    """ GET /api/v1/users/me
+    Return:
+      - the authenticated User object JSON represented
+      - 404 if no User is authenticated
+    """
+    if request.current_user is None:
+        abort(404)
+    return jsonify(request.current_user.to_json())
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
