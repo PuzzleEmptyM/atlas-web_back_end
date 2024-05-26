241aad6f40010997f9030687aab0868cf269aaab
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-05-26 17:29:25 -0500

    update password route

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -104,5 +104,23 @@ def reset_password():
         abort(403)
 
 
+@app.route("/reset_password", methods=["PUT"])
+def update_password():
+    """
+    PUT /reset_password
+    Update the user's password using the reset token
+    Return: JSON payload indicating success or failure
+    """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
