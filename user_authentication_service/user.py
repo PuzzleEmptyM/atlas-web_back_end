364351fc20bc8a8a91b68c1475a850cb5ded63a9
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-05-26 16:51:27 -0500

    pycodestyle

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -8,12 +8,13 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
     """
     User class represents the users table in the database
     """
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
