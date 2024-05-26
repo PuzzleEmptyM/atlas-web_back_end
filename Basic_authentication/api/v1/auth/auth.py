5a55be811da6c9ffbe2430b5b13ac9a20d479618
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-05-26 12:20:22 -0500

    require_auth method

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,22 +8,45 @@ from typing import List, TypeVar
 
 class Auth:
     """
-    API authentication
+    Class to manage the API authentication
     """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        Returns False for now - path and excluded_paths will be used later.
+        Determines if a path requires authentication
+        Arg1: path - str; The path to check
+        Arg2: excluded_paths - str; list of paths that don't require auth
+        Return: bool; True if authentication is required, False otherwise
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path[-1] != '/':
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] != '/':
+                excluded_path += '/'
+            if path == excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
-        Returns None - request will be the Flask request object.
+        Returns the authorization header from the request
+        Arg: request - Flask request object; The Flask request object
+        Return: str - The authorization header, or None if not present
         """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
-        Returns None - request will be the Flask request object.
+        Returns the current user from the request
+        Arg: request - Flask request object; The Flask request object
+        Return: TypeVar; The current user, or None if not present
         """
         return None
