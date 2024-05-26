83f17db141e7c5a55044cf741dbad4e4460905b7
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-05-26 14:40:32 -0500

    updated session auth

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,8 @@
 Session authentication module
 """
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
-
 
 class SessionAuth(Auth):
     """ Session authentication class """
@@ -26,3 +26,15 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Return a User instance based on a cookie value """
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+
+        return User.get(user_id)
