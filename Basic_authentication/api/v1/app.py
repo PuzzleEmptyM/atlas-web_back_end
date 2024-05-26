ac300530d1fdafbacafd1f3b6b8a301c5f758b1b
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-05-26 12:47:58 -0500

    BasicAuth class that inherits from Auth

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -16,7 +16,10 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv("AUTH_TYPE", None)
 
-if AUTH_TYPE == "auth":
+if AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic Authentication Module
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class that inherits from Auth
+    """
+    pass
