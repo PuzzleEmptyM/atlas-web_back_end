8650fd408bcec3be5c7b137b9b3cd36fd174785a
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-05-25 21:10:32 -0500

    pycode fixes

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -8,18 +8,9 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """
     Arg: password - string; password to hash
-    
+
     Return: the salted hashed password as a byte string
     """
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed
-
-
-
-
-
-
-
-
-
