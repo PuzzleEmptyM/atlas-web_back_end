f988eded84e3506bedff78fa24d166056d36b202
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-05-26 15:30:34 -0500

    pycode??????

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -60,7 +60,7 @@ def before_request():
         return
 
     if auth.authorization_header(request) is None and\
-        auth.session_cookie(request) is None:
+            auth.session_cookie(request) is None:
         abort(401)
 
     request.current_user = auth.current_user(request)
