6b643db2d4a0f69e80fc760e7d4a948d10394937
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-05-25 21:08:13 -0500

    moved to correct file

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to hash passwords.
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Arg: password - string; password to hash
+    
+    Return: the salted hashed password as a byte string
+    """
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
+
+
+
+
+
+
+
+
+
