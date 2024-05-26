863d7241700e3425de09191a3f14dcc5affb8aec
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-05-26 16:34:10 -0500

    destroy session added

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -39,3 +39,19 @@ class SessionAuth(Auth):
             return None
 
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """ Deletes the user session / logs out the user """
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            return False
+
+        del self.user_id_by_session_id[session_id]
+        return True
