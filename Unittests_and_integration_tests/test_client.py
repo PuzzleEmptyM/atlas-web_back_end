0a0165327da3cc9ef0a3c923278d020e06939281
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-30 11:36:44 -0500

    Test that GithubOrgClient.public_repos returns the expected

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -53,6 +53,35 @@ class TestGithubOrgClient(unittest.TestCase):
         result = client._public_repos_url
         self.assertEqual(result, payload["repos_url"])
 
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """
+        Test that GithubOrgClient.public_repos returns the expected
+        list of repositories.
+
+        Arg1: mock_get_json - Mock; Mocked get_json function.
+
+        Return: None
+        """
+        repos_payload = [
+            {"name": "repo1"},
+            {"name": "repo2"}
+        ]
+        mock_get_json.return_value = repos_payload
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = (
+                "https://api.github.com/orgs/google/repos")
+
+            client = GithubOrgClient("google")
+            result = client.public_repos()
+
+            self.assertEqual(result, ["repo1", "repo2"])
+            mock_public_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with(
+                "https://api.github.com/orgs/google/repos")
+
 
 if __name__ == "__main__":
     unittest.main()
