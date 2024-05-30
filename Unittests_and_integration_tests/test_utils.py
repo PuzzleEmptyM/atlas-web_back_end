2f7a6c24baec93025a873abf0564431cde7e68ef
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-30 10:29:27 -0500

    Test that a_property returns the correct result and a_method is called only once.

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -8,6 +8,7 @@ from parameterized import parameterized
 from unittest.mock import patch, Mock
 from utils import access_nested_map
 from utils import get_json
+from utils import memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -75,6 +76,41 @@ class TestGetJson(unittest.TestCase):
             result = get_json(test_url)
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    TestMemoize class to test the memoize decorator from utils module.
+    """
+    def test_memoize(self):
+        """
+        Test that a_property returns the correct result
+        and a_method is called only once.
+
+        Return: None
+        """
+
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass,
+                          'a_method', return_value=42) as mock_a_method:
+            test_instance = TestClass()
+            # Call a_property twice
+            result1 = test_instance.a_property
+            result2 = test_instance.a_property
+
+            # Verify the results are correct
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+
+            # Verify a_method was called only once
+            mock_a_method.assert_called_once()
 
 
 if __name__ == "__main__":
