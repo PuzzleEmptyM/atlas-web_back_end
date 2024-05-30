336c2ac0886171ab5cf35a1a14b12f410dcfff5c
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-30 10:22:23 -0500

    Test that get_json returns the expected result.

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,9 @@ Unit tests for utils.access_nested_map function.
 
 import unittest
 from parameterized import parameterized
+from unittest.mock import patch, Mock
 from utils import access_nested_map
+from utils import get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -47,6 +49,32 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), repr(path[-1]))
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    TestGetJson class to test the get_json function from utils module.
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """
+        Test that get_json returns the expected result.
+
+        Arg1: test_url - str; The URL to pass to get_json.
+        Arg2: test_payload - dict; The expected payload returned by get_json.
+
+        Return: None
+        """
+        with patch('utils.requests.get') as mock_get:
+            mock_get.return_value = Mock(**{'json.return_value': test_payload})
+
+            result = get_json(test_url)
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
