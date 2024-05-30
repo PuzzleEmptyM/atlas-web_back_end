c6a100b456774345c75ccaafdd73bfc04b944d04
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-29 20:12:03 -0500

    forgot decorator

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -31,6 +31,10 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
     def test_access_nested_map_exception(self, nested_map, path):
         """
         Test that access_nested_map raises KeyError for invalid paths.
