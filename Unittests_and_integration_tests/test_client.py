9080d02819620ef577ac7a59740bc7ea6a7629cb
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-05-30 11:32:50 -0500

    Test that GithubOrgClient._public_repos_url returns the

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@ Unit tests for GithubOrgClient class.
 """
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -30,12 +30,28 @@ class TestGithubOrgClient(unittest.TestCase):
         Return: None
         """
         mock_get_json.return_value = expected
-        
         client = GithubOrgClient(org_name)
         result = client.org()
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(result, expected)
+
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """
+        Test that GithubOrgClient._public_repos_url returns the
+        expected value based on mocked org.
+
+        Arg1: mock_org - PropertyMock; Mocked property for GithubOrgClient.org.
+
+        Return: None
+        """
+        payload = {"repos_url": "https://api.github.com/orgs/google/repos"}
+        mock_org.return_value = payload
+
+        client = GithubOrgClient("google")
+        result = client._public_repos_url
+        self.assertEqual(result, payload["repos_url"])
 
 
 if __name__ == "__main__":
