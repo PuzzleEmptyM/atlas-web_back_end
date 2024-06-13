09c86667941bc4d5453acd546cf7d32fafb46674
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-06-12 20:39:04 -0500

    type-annotate fix

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,13 +4,14 @@ Main file
 """
 import redis
 import uuid
+from typing import Union
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: str | bytes | int | float) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
