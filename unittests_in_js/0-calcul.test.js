e5a8adb639378c7b4d7137b2727344117c1d1609
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-06-19 21:07:26 -0500

    test for caculate

---
+++ unittests_in_js/0-calcul.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it('should return 4 when inputs are 1 and 3', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('should return 5 when inputs are 1 and 3.7', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return 5 when inputs are 1.2 and 3.7', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return 6 when inputs are 1.5 and 3.7', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should return 0 when inputs are 0 and 0', () => {
+    assert.strictEqual(calculateNumber(0, 0), 0);
+  });
+
+  it('should return -2 when inputs are -1.2 and -0.8', () => {
+    assert.strictEqual(calculateNumber(-1.2, -0.8), -2);
+  });
+});
