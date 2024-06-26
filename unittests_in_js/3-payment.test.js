558dd5feb6a06e4d9d1a2721c3ef67473f073927
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-06-26 17:00:45 -0500

    tests for new method

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -1,23 +1,12 @@
-// 3-payment.test.js
 const sinon = require('sinon');
 const { expect } = require('chai');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToApi', () => {
-  let spy;
-
-  beforeEach(() => {
-    spy = sinon.spy(Utils, 'calculateNumber');
-  });
-
-  afterEach(() => {
-    spy.restore();
-  });
-
-  it('should call Utils.calculateNumber with SUM, 100, 20', () => {
-    sendPaymentRequestToApi(100, 20);
-    expect(spy.calledOnce).to.be.true;
-    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
-  });
+describe('sendPaymentRequestToApi', function () {
+    var spy = sinon.spy(Utils, 'calculateNumber');;
+    it('makes sure the math is the same used as the utils method', function () {
+        sendPaymentRequestToApi(100, 20);
+        expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    });
 });
