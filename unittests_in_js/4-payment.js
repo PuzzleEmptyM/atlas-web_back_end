b16b526a4e46e6edc5a6cf0586e94d8f6f2ec502
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-06-26 16:58:05 -0500

    hopefully fixed assertions

--- unittests_in_js/4-payment.js
+++ unittests_in_js/4-payment.js
@@ -4,6 +4,7 @@ const Utils = require('./utils');
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${total}`);
+  return total;
 }
 
 module.exports = sendPaymentRequestToApi;
