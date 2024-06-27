f3cf52ae884d5f2697004be3955975f1a19878dc
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-06-27 11:16:51 -0500

    fixed wording

--- queuing_system_in_js/6-job_creator.js
+++ queuing_system_in_js/6-job_creator.js
@@ -3,8 +3,8 @@ import kue from 'kue';
 const queue = kue.createQueue();
 
 const jobData = {
-  phoneNumber: '1234567890',
-  message: 'This is a test notification message'
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account'
 };
 
 const job = queue.create('push_notification_code', jobData)
