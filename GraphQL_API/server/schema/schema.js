97332efab16c4088799bb6cc0d88e6f88da4c2f9
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-07-03 11:03:00 -0500

    new query with dummy data

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -4,6 +4,22 @@ const {
   GraphQLInt,
   GraphQLSchema,
 } = require('graphql');
+const _ = require('lodash');
+
+const tasks = [
+  {
+    id: '1',
+    title: 'Create your first webpage',
+    weight: 1,
+    description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
+  },
+  {
+    id: '2',
+    title: 'Structure your webpage',
+    weight: 1,
+    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
+  },
+];
 
 const TaskType = new GraphQLObjectType({
   name: 'Task',
@@ -22,12 +38,7 @@ const RootQuery = new GraphQLObjectType({
       type: TaskType,
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
-        return {
-          id: args.id,
-          title: 'Example Task',
-          weight: 10,
-          description: 'This is an example task description',
-        };
+        return _.find(tasks, { id: args.id });
       },
     },
   },
